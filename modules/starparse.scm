678bebaa62a5ca4d2554946e5a5b590af41ba76f
Greg Benison <gbenison@gmail.com>
2007-04-15 17:32:31 -0700

    added convenient bmrb->hash function for organizing assignments

--- modules/starparse.scm
+++ modules/starparse.scm
@@ -1,10 +1,39 @@
 
 (define-module (starparse)
-  #:export (star-parse read-bmrb-shifts)
+  #:export (star-parse
+	    read-bmrb-shifts
+	    bmrb->hash)
   #:use-module (ice-9 regex))
 
 (load-extension "libstarparse" "starparse_init")
 
+(define (bmrb->hash fname)
+  (let ((residues (make-hash-table 31))
+	(this-residue #f)
+	(atom-value #f)
+	(atom-name #f))
+    (define (ensure-hash resid)
+      (let ((result (hash-ref residues resid)))
+	(if result
+	    result
+	    (hash-set! residues resid (make-hash-table 5)))))
+    (star-parse fname #f
+		(lambda (name value)
+		  (case name
+		    ((Residue_seq_code Atom_chem_shift.Auth_seq_ID)
+		     (set! this-residue (ensure-hash value)))
+		    ((Atom_name Atom_chem_shift.Atom_ID Atom_chem_shift.Auth_atom_ID)
+		     (set! atom-name (string->symbol value)))
+		    ((Chem_shift_value Atom_chem_shift.Val)
+		     (set! atom-value value)))
+		  (if (and this-residue atom-name atom-value)
+		      (begin (hash-set! this-residue atom-name atom-value)
+			     (set! this-residue #f)
+			     (set! atom-name #f)
+			     (set! atom-value #f)))))
+    residues))
+
+;; deprecated in favor of bmrb->hash
 (define (read-bmrb-shifts fname)
   (let ((result (list))
 	(entry (list)))
